19872b207948304fe63b3c8342d9123b18ba9667
vanparfait <nguemechiaparfait@gmail.com>
2024-06-21 08:35:38 +0100

    mise en place du composant Gradient et de quelques fonctionnalites

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Gradient from "./Gradient";
+
 function App() {
-  return <div>bbbbbbbbbb</div>;
+  return (
+    <div className="relative  text-slate-100 max-w-xl mx-auto mt-20 p-4 border border-slate-400 flex">
+      <div className="w-1/2 p-4 pr-8">
+        <h1 className="text-center text-xl">Gradient Generator</h1>
+        <p className="text-center mb-6">Bring styles to your apps</p>
+        <p className="mb-1">color, min2 , max5</p>
+        <div className="flex mb-2">
+          <input type="color" />
+          <input type="color" />
+        </div>
+        <div className="mb-5">
+          <button>+</button>
+          <button>-</button>
+        </div>
+        <p>Pick and change a color position's</p>
+        {/* select color */}
+
+        <p>color's position</p>
+
+        {/* range color */}
+        <p>Gradient global angle</p>
+        {/* range angle */}
+
+        {/* open modal button */}
+      </div>
+      <Gradient />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Gradient.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Gradient = () => {
+  return (
+    <div className="w-1/2 border-4 border-slate-200 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+  );
+};
+
+export default Gradient;
